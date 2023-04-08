46ea1b72a755ac6ba1cf5a52f8035184ddc0ad57
lambert0422 <li2442264967@gmail.com>
2023-04-08 12:40:19 +0100

    Test ZBP

--- Kwant_Class_latest_ROG.py
+++ Kwant_Class_latest_ROG.py
@@ -1630,14 +1630,14 @@ DavidPot = False
 RefName = '/mnt/d/OneDrive/Desktop2/iCloud_Desktop/NN_backup/Reference/ReferData.xlsx'
 
 mu_N_list = [10e-3]
-mu_SC_list = [30e-3]
+mu_SC_list = [10e-3]
 # E_excited_list = [0.023,0.024]
 # E_excited_list = [0]
-E_excited_list = np.round(np.arange(-5e-3,5e-3,10e-5),6)
+E_excited_list = np.round(np.arange(-2e-3,2e-3,10e-5),6)
 # TeV_list = [1]
 # TeV_T_list = [0.5]
-TeV_list = [7e-3]
-TeV_T_list = [7e-3]
+TeV_list = [10e-3]
+TeV_T_list = [10e-3]
 # mu_N_list = [1e-3,2e-3]
 # mu_SC_list = [1e-3,2e-3]
 # # E_excited_list = [0.023,0.024]
@@ -1663,6 +1663,8 @@ else:
 
 # np.round(np.arange(0.5,-1.2,-0.01),3),
 delta_list = [6.5e-4] # in eV
+
+delta_list = np.logspace(np.log10(5e-6), np.log10(5e-5), num=10)
 # delta_list = [0.1] # in eV
 VGate_shift_list = [0]
 
@@ -1678,6 +1680,8 @@ else:
     NName = ''
 for DELTA in delta_list:
     for Vg_s in VGate_shift_list:
+        E_excited_list = np.arange(-50*DELTA,50*DELTA, 20*DELTA/100)
+
         B = Kwant_SSeS(NextNanoName=NName,W_r = 600, DavidPot=True, W_g=500, S_g=300, D_2DEG=250,
                        V_A=[0], TStrength=TStrength_list,
                        PeriBC=PeriBC_list, Tev=TeV_list,Tev_Tunnel=TeV_T_list,
