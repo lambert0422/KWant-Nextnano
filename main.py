15efcdbfd8d8798a073da21c34e22d5a1a02f356
lambert0422 <li2442264967@gmail.com>
2023-04-29 17:43:59 +0100

    Update

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ DavidPot = True
 
 RefName = '/mnt/d/OneDrive/Desktop2/iCloud_Desktop/NN_backup/Reference/ReferData.xlsx'
 
-mu_N_list = [5e-3,10e-3]
-mu_SC_list = np.arange(0,20e-3,100e-5)
+mu_N_list = [4e-3,5e-3,10e-3]
+mu_SC_list = [4e-3,5e-3,10e-3]
 # E_excited_list = [0.023,0.024]
 # E_excited_list = [0]
 E_excited_list = np.round(np.arange(-2e-3,2e-3,10e-5),6)
 # TeV_list = [1]
 # TeV_T_list = [0.5]
-TeV_list = [7e-3,10e-3,15e-3]
-TeV_T_list = [7e-3,10e-3,15e-3]
+TeV_list = [7e-3]
+TeV_T_list = [7e-3]
 # mu_N_list = [1e-3,2e-3]
 # mu_SC_list = [1e-3,2e-3]
 # # E_excited_list = [0.023,0.024]
@@ -60,17 +60,25 @@ else:
    
 for DELTA in delta_list:
     for Vg_s in VGate_shift_list:
-        E_excited_list = np.arange(-10*DELTA,10*DELTA, 20*DELTA/100)
-
+        E_excited_list = np.arange(-8*DELTA,8*DELTA, 8*DELTA/100)
+        B = KC.Kwant_SSeS(NextNanoName=NName, W_r=400, DavidPot=DavidPot, W_g=500, S_g=300, D_2DEG=250,
+                          V_A=[0], TStrength=TStrength_list,
+                          PeriBC=PeriBC_list, Tev=TeV_list, Tev_Tunnel=TeV_T_list,
+                          E_excited=E_excited_list, SNjunc=SNjunc_list,
+                          ProOn=ProximityOn_list, BField=[0],
+                          ShowDensity=ShowDensity, Phase=[np.pi / 4],
+                          SaveNameNote='', SeriesR=500,
+                          muN=mu_N_list, DefectAmp=0, CombineMu=False, CombineTev=True,
+                          muSC=mu_SC_list, delta=DELTA, VGate_shift=Vg_s, SwpID="E", PlotbeforeFigures=1)
         B = KC.Kwant_SSeS(NextNanoName=NName,W_r = 600, DavidPot=DavidPot, W_g=500, S_g=300, D_2DEG=250,
                        V_A=[0], TStrength=TStrength_list,
                        PeriBC=PeriBC_list, Tev=TeV_list,Tev_Tunnel=TeV_T_list,
                        E_excited=E_excited_list, SNjunc=SNjunc_list,
-                       ProximityOn=ProximityOn_list,BField=[0],Dict = Dict, VgList = VgList,
-                       ShowDensity=ShowDensity,phi=[np.pi/4],
+                       ProOn=ProximityOn_list,BField=[0],
+                       ShowDensity=ShowDensity,Phase=[np.pi/4],
                        SaveNameNote='',SeriesR = 500,
-                       mu_N=mu_N_list, DefectAmp=0,CombineMu=False,CombineTev=True,
-                       mu_SC=mu_SC_list, delta=DELTA, VGate_shift=Vg_s,SwpID = "E",PlotbeforeFigures=20)
+                       muN=mu_N_list, DefectAmp=0,CombineMu=False,CombineTev=True,
+                       muSC=mu_SC_list, delta=DELTA, VGate_shift=Vg_s,SwpID = "E",PlotbeforeFigures=1)
 
 
 
