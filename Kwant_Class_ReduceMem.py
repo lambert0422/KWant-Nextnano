Fix the E_excited normalization

Kwant_Class_ReduceMem.py
@@ -968,13 +968,13 @@ class Kwant_SSeS():
         if self.SwpID == 'E':
             # ax0.axvline(x=self.delta, color='r')
             # ax0.axvline(x=-self.delta, color='r')
-            ax0.axvline(x=self.delta * 2, color='b')
-            ax0.axvline(x=-self.delta * 2, color='b')
-            ax0.text(x=self.Delta_induced * 2, y=0, s=str(np.round(self.Delta_induced * 1e6, 3)))
+            ax0.axvline(x=self.delta * 2/self.t, color='b')
+            ax0.axvline(x=-self.delta * 2/self.t, color='b')
+            ax0.text(x=self.Delta_induced * 2/self.t, y=0, s=str(np.round(self.Delta_induced * 1e6, 3)))
             # ax0.axvline(x=self.Delta_induced, color='r')
             # ax0.axvline(x=-self.Delta_induced, color='r')
-            ax0.axvline(x=self.Delta_induced * 2, color='b')
-            ax0.axvline(x=-self.Delta_induced * 2, color='b')
+            ax0.axvline(x=self.Delta_induced * 2/self.t, color='b')
+            ax0.axvline(x=-self.Delta_induced * 2/self.t, color='b')
         if self.ReferenceData != None:
             ax0.plot(self.referdata.Vg1, self.referdata.G1, self.referdata.Vg2, self.referdata.G2)
         ax0.legend()
@@ -1184,7 +1184,7 @@ class Kwant_SSeS():
     def Run_sweep(self):
 
         def Delta_0_dis(x, y):
-            A = self.delta / self.t
+
             Square = np.heaviside(y, 0) - np.heaviside(y - self.W, 1)
             Delta_Spatial = self.delta * (1 - np.heaviside(y, 0)) + \
                             self.delta * np.heaviside(y - self.W, 1)
@@ -1310,6 +1310,11 @@ class Kwant_SSeS():
         print("--------------------------- Start Sweep -----------------------------------", end='\r')
 
         for self.SN, self.PB, self.ProximityOn, self.t, self.t_Tunnel in self.comb_change:
+            now = datetime.now()
+            DateLocal= now.strftime("%YY%mM%dD")
+            TimeLocal = now.strftime("%Hh%Mm%Ss")
+
+            self.LocalSave = DateLocal+'-'+TimeLocal
 
             sys = self.make_system()
             self.DefOutputMap()
@@ -1460,11 +1465,11 @@ class Kwant_SSeS():
                         try:
 
                             self.Gen_Site_Plot(sys, params)
-                            self.fig.savefig(self.SAVEFILENAME + '_' + str(VSwp) + "Sites.png")
+                            self.fig.savefig(self.SAVEFILENAME +self.LocalSave+ '_' + str(VSwp) + "Sites.png")
                             if self.ShowDensity == 1:
                                 self.fig.show()
                             self.Gen_Ana_Plot()
-                            self.fig.savefig(self.SAVEFILENAME + '_' + str(VSwp) + "Ana.png")
+                            self.fig.savefig(self.SAVEFILENAME +self.LocalSave+ '_' + str(VSwp) + "Ana.png")
                             if self.ShowDensity == 1:
                                 self.fig.show()
                         except:
@@ -1574,7 +1579,7 @@ class Kwant_SSeS():
                     round(self.SeriesR, 3)) + '.txt', init=False, newcol=Data_R)
 
         self.Gen_Conduct_Plot(self.VarSwp, self.conductances, self.SwpID)
-        self.fig.savefig(self.SAVEFILENAME + "Conductance.png")
+        self.fig.savefig(self.SAVEFILENAME +self.LocalSave+ "-Conductance.png")
 
         if Plot == 1:
             self.fig.show()
@@ -1614,7 +1619,7 @@ class Kwant_SSeS():
 
 
             self.Gen_Conduct_Plot(self.VarSwp, self.conductances2, self.SwpID)
-            self.fig.savefig(self.SAVEFILENAME + "N-Ree+Reh.png")
+            self.fig.savefig(self.SAVEFILENAME+self.LocalSave + "-N-Ree+Reh.png")
             if Plot == 1:
                 self.fig.show()
 

main.py
@@ -13,10 +13,13 @@ now = datetime.now()
 Date = now.strftime("%YY%mM%dD")
 Time = now.strftime("%Hh%Mm%Ss")
 master_file_path = __file__
-RefName = '/mnt/d/OneDrive/Desktop2/iCloud_Desktop/NN_backup/Reference/ReferData.xlsx'
+RefName = None
 
 mu_N_list = [3.2e-3]
 mu_SC_list = [3.25e-3]
+
+mu_N_list = [0.1e-3,1e-3,1.5e-3,2e-3,5e-3,10e-3,15e-3]
+mu_SC_list = [0.1e-3,1e-3,1.5e-3,2e-3,5e-3,10e-3,15e-3]
 E_excited_list = [0.035]
 # # E_excited_list = [0]
 # E_excited_list = np.round(np.arange(-2e-3,2e-3,10e-5),6)
@@ -40,16 +43,12 @@ ProximityOn_list = [1]
 lenswp = len(mu_SC_list) * len(mu_N_list) * len(E_excited_list) * len(TeV_list) * len(PeriBC_list) * len(
     TStrength_list) * len(SNjunc_list) * len(ProximityOn_list)
 
-if ~DavidPot & lenswp > 1:
-    ShowDensity = False
-elif DavidPot & lenswp * len(W_g_list) * len(S_g_list) > 1:
-    ShowDensity = False
-else:
-    ShowDensity = True
+ShowDensity = False
+
 
 # np.round(np.arange(0.5,-1.2,-0.01),3),
 # delta_list = [6.5e-4] # in eV
-delta_list = [6.5e-4,6.5e-5] # in eV
+delta_list = [6.5e-4] # in eV
 # delta_list = [6.4e-6]
 # delta_list = [0.1] # in eV
 VGate_shift_list = [0]
@@ -64,15 +63,28 @@ else:
     NName = '/mnt/d/OneDrive/Desktop2/iCloud_Desktop/NN_backup/UpdateSiDopedLayerThickness/2023Y03M23D-17h59m57s'
 for DELTA in delta_list:
     for Vg_s in VGate_shift_list:
-        E_excited_list = np.arange(-10*DELTA, 10*DELTA, 10*DELTA/50)
-        B = KC.Kwant_SSeS(NextNanoName=NName,Masterfilepath = master_file_path,W_r = 0, DavidPot=DavidPot, W_g=500, S_g=300, D_2DEG=250,
-                       V_A=[0], TStrength=TStrength_list,a=20,WSC = 500,
-                       PeriBC=PeriBC_list, Tev=TeV_list,Tev_Tunnel=TeV_T_list,
-                       E_excited=E_excited_list, SNjunc=SNjunc_list,
-                       ProOn=ProximityOn_list,BField=[0],
-                       ShowDensity=ShowDensity,Phase=[0],
-                       SaveNameNote=NName,SeriesR = 500,DateT=Date,TimeT = Time,MasterMultiRun=MMR,
-                       muN=mu_N_list, DefectAmp=0,DefectNumPer=0,CombineMu=False,CombineTev=True,
-                       muSC=mu_SC_list, delta=DELTA, VGate_shift=Vg_s,SwpID = "E",PlotbeforeFigures=1,PlotbeforeFigures_Ana=40)
+        E_excited_list = np.arange(-0.3, 0.3, 0.2/100)
+        B = KC.Kwant_SSeS(NextNanoName=NName, Masterfilepath=master_file_path, ReferenceData=RefName, W_r=1500,
+                          DavidPot=DavidPot, W_g=500, S_g=300, D_2DEG=250,
+                          V_A=[0], TStrength=TStrength_list,TeV_Normal=True,
+                          PeriBC=PeriBC_list, Tev=TeV_list, Tev_Tunnel=TeV_T_list,
+                          E_excited=E_excited_list, SNjunc=SNjunc_list,
+                          ProOn=ProximityOn_list, BField=[0], a=25,
+                          ShowDensity=ShowDensity, Phase=[np.pi / 4],
+                          SaveNameNote=NName, SeriesR=500, DateT=Date, TimeT=Time, MasterMultiRun=MMR,
+                          muN=mu_N_list, DefectAmp=0, DefectNumPer=0, CombineMu=True, CombineTev=False,
+                          muSC=mu_SC_list, delta=DELTA, VGate_shift=Vg_s, SwpID="E", PlotbeforeFigures=1,
+                          PlotbeforeFigures_Ana=20)
+        B = KC.Kwant_SSeS(NextNanoName=NName, Masterfilepath=master_file_path, ReferenceData=RefName, W_r=1500,
+                          DavidPot=DavidPot, W_g=500, S_g=300, D_2DEG=250,WSC=500,
+                          V_A=[0], TStrength=TStrength_list,TeV_Normal=True,
+                          PeriBC=PeriBC_list, Tev=TeV_list, Tev_Tunnel=TeV_T_list,
+                          E_excited=E_excited_list, SNjunc=SNjunc_list,
+                          ProOn=ProximityOn_list, BField=[0], a=25,
+                          ShowDensity=ShowDensity, Phase=[np.pi / 4],
+                          SaveNameNote=NName, SeriesR=500, DateT=Date, TimeT=Time, MasterMultiRun=MMR,
+                          muN=mu_N_list, DefectAmp=0, DefectNumPer=0, CombineMu=True, CombineTev=False,
+                          muSC=mu_SC_list, delta=DELTA, VGate_shift=Vg_s, SwpID="E", PlotbeforeFigures=1,
+                          PlotbeforeFigures_Ana=20)
 
 

main_step.py
@@ -36,7 +36,7 @@ TeV_T_list = [4.5e-3]
 PeriBC_list = [0]
 # TStrength_list = np.round(np.arange(0,2,0.04),5)
 TStrength_list = [0]
-SNjunc_list = ['SN']
+SNjunc_list = ['SNS']
 ProximityOn_list = [1]
 
 lenswp = len(mu_SC_list) * len(mu_N_list) * len(E_excited_list) * len(TeV_list) * len(PeriBC_list) * len(
@@ -87,7 +87,7 @@ else:
     #
     # NName_list.append('/mnt/d/OneDrive/Desktop2/iCloud_Desktop/NN_backup/UpdateSiDopedLayerThickness/2023Y05M22D-16h26m01s')  # with 2 / 2.3 surface charge
     # NName_list.append('/mnt/d/OneDrive/Desktop2/iCloud_Desktop/NN_backup/UpdateSiDopedLayerThickness/2023Y05M24D-09h15m55s')  # with 2 / 2.5 surface charge
-    # NName_list.append('/mnt/d/OneDrive/Desktop2/iCloud_Desktop/NN_backup/UpdateSiDopedLayerThickness/2023Y05M28D-12h21m17s')  # with 1.8 / 3 surface charge
+    # NName_list.append('/mnt/d/OneDrive/Desktop2/iCloud_Desktop/NN_backup/UpdateSiDopedLayerThickness/2023Y05M28D-12h21m17s')  # with 1.8 / 3 surface charge(Best)
 for DELTA in delta_list:
     for Vg_s in VGate_shift_list:
         for NName in NName_list: