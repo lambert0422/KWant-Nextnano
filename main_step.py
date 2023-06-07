9d35e40930ad91e7c810739563e803458a55077d
lambert0422 <li2442264967@gmail.com>
2023-06-07 17:36:27 +0100

    keep update

--- main_step.py
+++ main_step.py
@@ -17,21 +17,14 @@ ShowDensity = False
 master_file_path = __file__
 RefName = '/mnt/d/OneDrive/Desktop2/iCloud_Desktop/NN_backup/Reference/ReferData.xlsx'
 
-mu_N_list = [3.2e-3]
-mu_SC_list = [3.25e-3]
-E_excited_list = [0.035]
-# # E_excited_list = [0]
-# E_excited_list = np.round(np.arange(-2e-3,2e-3,10e-5),6)
-# TeV_list = [1]
-# TeV_T_list = [0.5]
-TeV_list = [4.5e-3]
-TeV_T_list = [4.5e-3]
-# mu_N_list = [1e-3,2e-3]
-# mu_SC_list = [1e-3,2e-3]
-# # E_excited_list = [0.023,0.024]
-# E_excited_list = [0.01]
-# TeV_list = [3e-3]
+mu_N_list = [3.3e-3]
+mu_SC_list = [3.3e-3]
+E_excited_list = [0.05]
 
+# TeV_list = [4.5e-3]
+# TeV_T_list = [4.5e-3]
+TeV_list = [4.2e-3,4.8e-3]
+TeV_T_list = [4.2e-3,4.8e-3]
 
 PeriBC_list = [0]
 # TStrength_list = np.round(np.arange(0,2,0.04),5)
@@ -92,11 +85,11 @@ for DELTA in delta_list:
     for Vg_s in VGate_shift_list:
         for NName in NName_list:
             B = KC.Kwant_SSeS(NextNanoName=NName,Masterfilepath = master_file_path,ReferenceData = RefName, W_r = 1500, DavidPot=DavidPot, W_g=500, S_g=300, D_2DEG=250,
-                           V_A=np.round(np.arange(0.5,-1.2,-0.01),3), TStrength=TStrength_list,
+                           V_A=np.round(np.arange(0.5,-1.2,-0.02),3), TStrength=TStrength_list,
                            PeriBC=PeriBC_list, Tev=TeV_list,Tev_Tunnel=TeV_T_list,
                            E_excited=E_excited_list, SNjunc=SNjunc_list,
                            ProOn=ProximityOn_list,BField=[0],a = 25,
-                           ShowDensity=ShowDensity,Phase=[np.pi/2,2.6],
+                           ShowDensity=ShowDensity,Phase=[2.6],
                            SaveNameNote=NName,SeriesR = 500,DateT=Date,TimeT = Time,MasterMultiRun=MMR,
                            muN=mu_N_list, DefectAmp=0,DefectNumPer=0,CombineMu=True,CombineTev=False,
                            muSC=mu_SC_list, delta=DELTA, VGate_shift=Vg_s,SwpID = "Vg",PlotbeforeFigures=1,PlotbeforeFigures_Ana=20)
